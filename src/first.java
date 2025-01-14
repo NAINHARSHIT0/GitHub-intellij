a33f40d3ab98429af5d346e9f3552cb3fbe19974
NAINHARSHIT0 <HARSHITNAIN0102@GMAIL.COM>
2025-01-14 10:37:37 +0530

    init commit

---
+++ src/first.java
@@ -0,0 +1,32 @@
+
+//Task 1 Display your name and complete address in different lines
+
+//public class first {
+//    public static void main(String[] args) {
+//        System.out.println("Name - Harshit");
+//        System.out.println("Age - 19");
+//        System.out.println("Country - India");
+//        System.out.println("City - Dehradun");
+//    }
+//}
+
+
+
+
+//Task 2 Design a visually appealing grade sheet
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Grade Sheet");
+        System.out.println("_________________");
+        System.out.println("Subject\t\tMarks");
+        System.out.println("_________________");
+        System.out.println("Maths\t\t90");
+        System.out.println("Science\t\t80");
+        System.out.println("English\t\t70");
+        System.out.println("_________________");
+        System.out.println("Total\t\t300");
+    }
+
+}
+
