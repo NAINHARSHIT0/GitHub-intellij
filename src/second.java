d130df05a61f2998639d0d6877d3db08d0e5b0fc
NAINHARSHIT0 <HARSHITNAIN0102@GMAIL.COM>
2025-01-28 10:39:49 +0530

    init commit

--- src/second.java
+++ src/second.java
@@ -39,27 +39,53 @@
 //Task - 3
 
 
+//public class second {
+//    public static void main(String[] args) {
+//
+//        if (args.length != 3) {
+//            System.out.println("Usage: java Calc <num1> <operator> <num2>");
+//            return;
+//        }
+//
+//        int num1 = Integer.parseInt(args[0]);
+//        char operator = args[1].charAt(0);
+//        int num2 = Integer.parseInt(args[2]);
+//
+//        if (operator == '+')
+//            System.out.println("Sum: " + (num1 + num2));
+//        else if (operator == '-')
+//            System.out.println("Difference: " + (num1 - num2));
+//        else if (operator == '*')
+//            System.out.println("Product: " + (num1 * num2));
+//        else if (operator == '/')
+//            System.out.println("Quotient: " + (num1 / num2));
+//        else
+//            System.out.println("Invalid operator!");
+//    }
+//}
+
+
+
+//Task- 4
+
+
+import java.util.Scanner;
+
 public class second {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        if (args.length != 3) {
-            System.out.println("Usage: java Calc <num1> <operator> <num2>");
-            return;
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
         }
 
-        int num1 = Integer.parseInt(args[0]);
-        char operator = args[1].charAt(0);
-        int num2 = Integer.parseInt(args[2]);
-
-        if (operator == '+')
-            System.out.println("Sum: " + (num1 + num2));
-        else if (operator == '-')
-            System.out.println("Difference: " + (num1 - num2));
-        else if (operator == '*')
-            System.out.println("Product: " + (num1 * num2));
-        else if (operator == '/')
-            System.out.println("Quotient: " + (num1 / num2));
-        else
-            System.out.println("Invalid operator!");
+        scanner.close();
     }
 }
