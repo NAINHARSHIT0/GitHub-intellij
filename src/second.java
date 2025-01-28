9cd877fc12b42f349e634213162fb3e59d6f4241
NAINHARSHIT0 <HARSHITNAIN0102@GMAIL.COM>
2025-01-28 11:10:16 +0530

    init commit

--- src/second.java
+++ src/second.java
@@ -69,23 +69,61 @@
 //Task- 4
 
 
+//import java.util.Scanner;
+//
+//public class second {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter a number: ");
+//        int number = scanner.nextInt();
+//
+//        if (number > 0) {
+//            System.out.println("The number is positive.");
+//        } else if (number < 0) {
+//            System.out.println("The number is negative.");
+//        } else {
+//            System.out.println("The number is zero.");
+//        }
+//
+//        scanner.close();
+//    }
+//}
+
+
+
+//Task - 5
+
+
 import java.util.Scanner;
 
 public class second {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt(); // Accepts the first integer
 
-        if (number > 0) {
-            System.out.println("The number is positive.");
-        } else if (number < 0) {
-            System.out.println("The number is negative.");
-        } else {
-            System.out.println("The number is zero.");
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt(); // Accepts the second integer
+
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt(); // Accepts the third integer
+
+        if (num1 >= num2) { // Checks if num1 is greater than or equal to num2
+            if (num1 >= num3) { // If num1 is also greater than or equal to num3
+                System.out.println("The greatest number is: " + num1);
+            } else { // If num3 is greater than num1
+                System.out.println("The greatest number is: " + num3);
+            }
+        } else { // If num2 is greater than num1
+            if (num2 >= num3) { // Checks if num2 is greater than or equal to num3
+                System.out.println("The greatest number is: " + num2);
+            } else { // If num3 is greater than num2
+                System.out.println("The greatest number is: " + num3);
+            }
         }
 
-        scanner.close();
+        scanner.close(); // Closes the scanner object
     }
 }
--- src/third.java
+++ src/third.java
@@ -1,39 +1,39 @@
 
 //sum of all integers between 10 and 950 divisible by both 6 and 9
 
-public class third{
-    public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 10; i <= 950; i++) {
-            if (i % 18 == 0) {
-                sum += i;
-            }
-        }
-        System.out.println("The sum of all integers between 10 and 950 divisible by both 6 and 9 is: " + sum);
-    }
-}
-
-
-
-
-
-//import java.util.Scanner;
-//
-//public class SumOfDigits {
+//public class third{
 //    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.print("Enter an integer: ");
-//        int number = scanner.nextInt();
 //        int sum = 0;
-//
-//        // Extract each digit and add it to the sum
-//        while (number != 0) {
-//            sum += number % 10; // Get the last digit
-//            number /= 10;       // Remove the last digit
+//        for (int i = 10; i <= 950; i++) {
+//            if (i % 18 == 0) {
+//                sum += i;
+//            }
 //        }
-//
-//        System.out.println("The sum of the digits is: " + sum);
-//        scanner.close();
+//        System.out.println("The sum of all integers between 10 and 950 divisible by both 6 and 9 is: " + sum);
 //    }
 //}
+
+
+
+
+
+import java.util.Scanner;
+
+public class third {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+        int sum = 0;
+
+
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        System.out.println("sum of digits is: " + sum);
+        scanner.close();
+    }
+}
