5790e6de1858f5842e49d42c249a3699df180c40
NAINHARSHIT0 <HARSHITNAIN0102@GMAIL.COM>
2025-02-04 11:05:18 +0530

    init commit

---
+++ src/fourth.java
@@ -0,0 +1,36 @@
+// Experiment 1
+
+public class fourth {
+    private String name;
+    private int age;
+
+    public fourth() {            //default constructor
+        this.name = "Default Name";
+        this.age = 00;
+    }
+
+    public fourth(String name, int age) {          //parameterised constructor
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        fourth student1 = new fourth();
+        System.out.println("Student 1 Details:");
+        student1.displayDetails();
+
+        System.out.println();
+
+        fourth student2 = new fourth("Aryan", 19);
+        System.out.println("Student 2 Details:");
+        student2.displayDetails();
+    }
+}
+
+
+// Experiment 2
